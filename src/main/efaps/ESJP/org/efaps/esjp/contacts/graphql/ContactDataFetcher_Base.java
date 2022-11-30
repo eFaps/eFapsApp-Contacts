8d62c39a619e583ca530589612686c99ef964077
Jan Moxter <jan@moxter.net>
2022-11-29 23:06:31 -0500

    Mapping for ContactDataFetcher

--- src/main/efaps/ESJP/org/efaps/esjp/contacts/graphql/ContactDataFetcher_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/contacts/graphql/ContactDataFetcher_Base.java
@@ -16,19 +16,25 @@
  */
 package org.efaps.esjp.contacts.graphql;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
 import org.efaps.admin.program.esjp.EFapsApplication;
 import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.Instance;
 import org.efaps.eql.EQL;
 import org.efaps.esjp.ci.CIContacts;
 import org.efaps.esjp.db.InstanceUtils;
+import org.efaps.graphql.definition.ObjectDef;
 
 import graphql.execution.DataFetcherResult;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLNamedType;
 
 @EFapsUUID("484b0053-6204-419e-873b-40b1d25ffa35")
 @EFapsApplication("eFapsApp-Contacts")
@@ -48,29 +54,50 @@ public abstract class ContactDataFetcher_Base
         } else if (contactObject instanceof String) {
             contactInst = Instance.get((String) contactObject);
         }
+
+        final var graphTypeName = ((GraphQLNamedType) _environment.getFieldType()).getName();
+        final Optional<ObjectDef> objectDefOpt = _environment.getGraphQlContext().getOrEmpty(graphTypeName);
+        final var keyMapping = new HashMap<String, String>();
+        if (objectDefOpt.isPresent()) {
+            final var objectDef = objectDefOpt.get();
+            final String[] words = { "idTypeKey", "idTypeName", "idNumber" };
+            for (final var child : _environment.getFieldType().getChildren()) {
+                final var fieldDef = objectDef.getFields().get(((GraphQLFieldDefinition) child).getName());
+                if (StringUtils.isNotEmpty(fieldDef.getSelect())
+                                && Arrays.stream(words).anyMatch(fieldDef.getSelect()::equals)) {
+                    keyMapping.put(fieldDef.getSelect(), fieldDef.getName());
+                }
+            }
+        }
+
         final Map<String, String> data = new HashMap<>();
         if (InstanceUtils.isType(contactInst, CIContacts.Contact)) {
-           final var evaluator = EQL.builder()
-               .print(contactInst)
-               .attribute(CIContacts.Contact.Name)
-               .clazz(CIContacts.ClassOrganisation).attribute(CIContacts.ClassOrganisation.TaxNumber).as("taxNumber")
-               .clazz(CIContacts.ClassPerson).attribute(CIContacts.ClassPerson.IdentityCard).as("identityCard")
-               .clazz(CIContacts.ClassPerson).linkto(CIContacts.ClassPerson.DOITypeLink)
-                   .attribute(CIContacts.AttributeDefinitionDOIType.Value).as("doiTypeValue")
-               .clazz(CIContacts.ClassPerson).linkto(CIContacts.ClassPerson.DOITypeLink)
-                   .attribute(CIContacts.AttributeDefinitionDOIType.Description).as("doiTypeDescr")
-               .evaluate();
-           data.put("name", evaluator.get(CIContacts.Contact.Name));
-           final String taxNumber = evaluator.get("taxNumber");
-           if (taxNumber != null) {
-               data.put("idTypeKey", "07");
-               data.put("idTypeName", "RUC");
-               data.put("idNumber", taxNumber);
-           } else {
-               data.put("idTypeKey", evaluator.get("doiTypeValue"));
-               data.put("idTypeName",  evaluator.get("doiTypeDescr"));
-               data.put("idNumber", evaluator.get("identityCard"));
-           }
+            final var evaluator = EQL.builder()
+                            .print(contactInst)
+                            .attribute(CIContacts.Contact.Name)
+                            .clazz(CIContacts.ClassOrganisation).attribute(CIContacts.ClassOrganisation.TaxNumber)
+                            .as("taxNumber")
+                            .clazz(CIContacts.ClassPerson).attribute(CIContacts.ClassPerson.IdentityCard)
+                            .as("identityCard")
+                            .clazz(CIContacts.ClassPerson).linkto(CIContacts.ClassPerson.DOITypeLink)
+                            .attribute(CIContacts.AttributeDefinitionDOIType.Value).as("doiTypeValue")
+                            .clazz(CIContacts.ClassPerson).linkto(CIContacts.ClassPerson.DOITypeLink)
+                            .attribute(CIContacts.AttributeDefinitionDOIType.Description).as("doiTypeDescr")
+                            .evaluate();
+            data.put("name", evaluator.get(CIContacts.Contact.Name));
+            final String taxNumber = evaluator.get("taxNumber");
+            if (taxNumber != null) {
+                data.put(keyMapping.containsKey("idTypeKey") ? keyMapping.get("idTypeKey") : "idTypeKey", "07");
+                data.put(keyMapping.containsKey("idTypeName") ? keyMapping.get("idTypeName") : "idTypeName", "RUC");
+                data.put(keyMapping.containsKey("idNumber") ? keyMapping.get("idNumber") : "idNumber", taxNumber);
+            } else {
+                data.put(keyMapping.containsKey("idTypeKey") ? keyMapping.get("idTypeKey") : "idTypeKey",
+                                evaluator.get("doiTypeValue"));
+                data.put(keyMapping.containsKey("idTypeName") ? keyMapping.get("idTypeName") : "idTypeName",
+                                evaluator.get("doiTypeDescr"));
+                data.put(keyMapping.containsKey("idNumber") ? keyMapping.get("idNumber") : "idNumber",
+                                evaluator.get("identityCard"));
+            }
         }
         return DataFetcherResult.newResult()
                         .data(data)
