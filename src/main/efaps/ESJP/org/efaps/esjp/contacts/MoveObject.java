35dfeb47aed74a93c179972e090ffbdb964db975
Jan Moxter <jan@moxter.net>
2024-03-26 14:36:56 -0500

    Headers

--- src/main/efaps/ESJP/org/efaps/esjp/contacts/MoveObject.java
+++ src/main/efaps/ESJP/org/efaps/esjp/contacts/MoveObject.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2011 The eFaps Team
+ * Copyright © 2003 - 2024 The eFaps Team (-)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,12 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Revision:        $Rev: 1 $
- * Last Changed:    $Date: 2011-06-13 15:23:17 -0500 (lun, 13 jun 2011) $
- * Last Changed By: $Author: Luis Estrada $
  */
-
 package org.efaps.esjp.contacts;
 
 import org.efaps.admin.program.esjp.EFapsApplication;
--- src/main/efaps/ESJP/org/efaps/esjp/contacts/listener/package-info.java
+++ src/main/efaps/ESJP/org/efaps/esjp/contacts/listener/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2015 The eFaps Team
+ * Copyright © 2003 - 2024 The eFaps Team (-)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,12 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Revision:        $Rev: 13356 $
- * Last Changed:    $Date: 2014-07-17 17:54:10 -0500 (Thu, 17 Jul 2014) $
- * Last Changed By: $Author: jan@moxter.net $
  */
-
 /**
  * TODO comment!
  *
